712ac8488f74d0f488b99343727cf7318ef01626
Brandon VanFossen <bgvfossen@gmail.com>
2024-04-07 11:12:23 -0400

    first comment

---
+++ index.js
@@ -0,0 +1,16 @@
+require('dotenv').config();
+const express = require('express');
+const path = require('path');
+const cors = require('cors');
+
+//initializing express app
+const app = express()
+
+
+port = 400
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/index.html'));
+})
+
+app.listen(port, console.log("server running on " + port))
