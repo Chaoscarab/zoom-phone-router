750214455fabf8d015f5a3cb23cb8c1536d47f46
Brandon VanFossen <bgvfossen@gmail.com>
2024-04-08 13:46:35 -0400

    comment

--- index.js
+++ index.js
@@ -15,23 +15,34 @@ const app = express()
 
 app.use(express.json({}))
 
+//CERTDIR
+///HTTPSPORT
+//SECRETKEY
+
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/index.html'));
+    console.log(process.env.HTTPSPORT)
 })
 
 
 app.post('/webhook', (req, res) => {
-    console.log(req.body)
-    res.status(200)
 
-    const hmac = crypto.createHmac('sha256', 'qs0NKiOvQG2w0-z6XY_2Ig');
-    data = hmac.update(req.body.payload.plainToken)
-    gen_hmac = data.digest('hex');
+    if(req.body.event === 'endpoint.url_validation'){
+            console.log(req.body)
+            res.status(200)
 
-    res.json({
-       "plainToken": req.body.payload.plainToken,
-       "encryptedToken": gen_hmac
-    })
+            const hmac = crypto.createHmac('sha256', 'qs0NKiOvQG2w0-z6XY_2Ig');
+            data = hmac.update(req.body.payload.plainToken)
+            gen_hmac = data.digest('hex');
+
+            res.json({
+            "plainToken": req.body.payload.plainToken,
+            "encryptedToken": gen_hmac
+            })
+    }
+
+    3
+    
 })
 https
     .createServer({
