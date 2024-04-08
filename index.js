aef9b3b89cf4c5ea18d4e6dc096f53a12f68ce34
Brandon VanFossen <bgvfossen@gmail.com>
2024-04-08 13:26:36 -0400

    first commit

--- index.js
+++ index.js
@@ -22,13 +22,13 @@ app.get('/', (req, res) => {
 
 app.post('/webhook', (req, res) => {
     console.log(req.body)
-    response.status(200)
+    res.status(200)
 
     const hmac = crypto.createHmac('sha256', 'qs0NKiOvQG2w0-z6XY_2Ig');
     data = hmac.update(request.body.payload.plainToken)
     gen_hmac = data.digest('hex');
 
-    response.json({
+    res.json({
        "plainToken": req.body.payload.plainToken,
        "encryptedToken": gen_hmac
     })
