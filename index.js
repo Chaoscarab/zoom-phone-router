7445b7f6c1cc7762f6ec9e9462b34e9366f9b3fe
Brandon VanFossen <bgvfossen@gmail.com>
2024-05-16 10:40:45 -0400

    added console.log for forwarded_by

--- index.js
+++ index.js
@@ -115,7 +115,7 @@ app.post('/webhook', (req, res) => {
         if(fetchObj === false){
 
         }else{
-            console.log(req.body.payload.object.caller)
+            console.log(req.body.payload)
             fetchFunc(fetchObj, process.env.ZOOMINBOUND)
         }
         res.status(200)
