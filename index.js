74c65d5f63130e5f8f982a267ae7fe4567f95462
Brandon VanFossen <bgvfossen@gmail.com>
2024-05-17 12:28:17 -0400

    remove mongo

--- index.js
+++ index.js
@@ -5,37 +5,16 @@ const cors = require('cors');
 const https = require('https');
 const fs = require('fs');
 const crypto = require('crypto');
-const { MongoClient, ServerApiVersion } = require('mongodb');
 
 
 //initializing express app
 const app = express()
 
-const uri = process.env.ATLASURI
-console.log(uri)
+
 
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
-});
-async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
-}
-run().catch(console.dir);
+
 
 
 app.use(express.json({}))
