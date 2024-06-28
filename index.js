4499750cda11f9b28eecc4132e192fd4de08d14a
Brandon VanFossen <bgvfossen@gmail.com>
2024-06-28 17:23:44 -0400

    message

--- index.js
+++ index.js
@@ -433,6 +433,7 @@ app.post('/app', async (req, res) => {
     try{
         const read = await readDoc({userId: userId})
         const object = await read[0]
+        console.log(typeof(object))
         console.log(object, 'object')
         var keys = Object.keys(object);
         console.log(keys)
