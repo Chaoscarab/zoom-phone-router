c23b3e0ef62e4d598c157314a18a72e10f4f385b
Brandon VanFossen <bgvfossen@gmail.com>
2024-06-30 17:08:47 -0400

    message

--- index.js
+++ index.js
@@ -483,7 +483,7 @@ app.post('/app', async (req, res) => {
         console.log(getContact)
         if(getContact.status === 200){
 
-            console.log(read.body.contact.customFields)
+            console.log(getContact.body.contact.customFields)
 
 
             
